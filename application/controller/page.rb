16275fce2191c2c414f59d6627c5c2ae1c2b30b2
Jeremy Bush <jeremybush@Macintosh-3.local>
2009-07-24 16:59:17 -0500

    adding more files.

--- application/controller/page.rb
+++ application/controller/page.rb
@@ -8,6 +8,14 @@ module Picombo
 				template.set('body', body.render(true))
 				template.render
 			end
+
+			def banned()
+				template = Picombo::View.new('template')
+				body = Picombo::View.new('page/banned')
+
+				template.set('body', body.render(true))
+				template.render
+			end
 		end
 	end
 end
