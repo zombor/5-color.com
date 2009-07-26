fd2b22747fcade26ee85dea0d2ef420a1375116d
Jeremy Bush <jeremybush@Macintosh-3.local>
2009-07-26 12:08:49 -0500

    trying out some admin stuff..

--- application/controller/admin.rb
+++ application/controller/admin.rb
@@ -12,7 +12,7 @@ module Picombo
 					template.render
 				else
 					Picombo::Input.instance.post.each do |card_id|
-						card = Picombo::Models::Banned.new(:id => card_id)
+						card = Picombo::Models::Banned[card_id]
 						
 						Picombo::Core.response(card.inspect)
 					end
